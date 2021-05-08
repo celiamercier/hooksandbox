48a17d9175dc52903ff3e9491e1e5eff8a001545
Célia Mercier <celia.mercier@mirakl.com>
2021-05-08 15:48:30 +0200

    add theme for useContext example

---
+++ src/components/theme/theme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const defaultTheme = {
+    primary: '#3E3E3E',
+};
+export const oceanTheme = {
+    primary: '#1780EA',
+};
+
+export const ThemeContext = React.createContext(defaultTheme);
